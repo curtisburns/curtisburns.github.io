377d74a09e375725e6200469894d385b797b617b
curtisburns <curtis.burns28@gmail.com>
2018-09-30 02:45:46 +0100

    playing around with scroll points - nav now highlights when section of page being viewed

---
+++ js/app.js
@@ -0,0 +1,51 @@
+window.onload = () => {
+
+  const about = document.querySelector('#about');
+  const projects = document.querySelector('#projects');
+  const skills = document.querySelector('#skills');
+  const aboutNav = document.querySelector('.about-nav');
+  const projectsNav = document.querySelector('.projects-nav');
+  const skillsNav = document.querySelector('.skills-nav');
+
+  const isInView = function(sectionId, nextSectionId) {
+    if (nextSectionId) {
+      if (sectionId.getBoundingClientRect().top < window.innerHeight && nextSectionId.getBoundingClientRect().top > window.innerHeight ) {
+        return true;
+      }
+    } else if (sectionId.getBoundingClientRect().top < window.innerHeight) {
+      return true;
+    } else
+      return false;
+  };
+
+
+
+
+  window.onscroll = () =>  {
+
+    switch(true) {
+      case(isInView(about, projects)):
+        console.log('ABOUT is active');
+        aboutNav.classList.add('active');
+        projectsNav.classList.remove('active');
+        skillsNav.classList.remove('active');
+        break;
+      case(isInView(projects, skills)):
+        console.log('PROJECTS is active');
+        aboutNav.classList.remove('active');
+        projectsNav.classList.add('active');
+        skillsNav.classList.remove('active');
+        break;
+      case(isInView(skills)):
+        console.log('SKILLS is active');
+        aboutNav.classList.remove('active');
+        projectsNav.classList.remove('active');
+        skillsNav.classList.add('active');
+        break;
+      default: console.log('Nothing is active');
+        aboutNav.classList.remove('active');
+        projectsNav.classList.remove('active');
+        skillsNav.classList.remove('active');
+    }
+  };
+};
